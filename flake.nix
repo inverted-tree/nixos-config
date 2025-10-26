d27f2858c317f72ad652ea3f5c344977eb66202e
Lukas Schröder <lukasscr@proton.me>
2025-10-26 10:00:14 +0100

    Restructure deployment naming scheme
    The deployments now get their names from the NATO phonetic alphabet.
    Deployment sites get greek letters as names, though don't need to be in
    order.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,13 @@
+# .    _                    ___ _
+#  ___|_|_ _    ___ ___ ___|  _|_|___
+# |   | |_|_|  |  _| . |   |  _| | . |
+# |_|_|_|_|_|  |___|___|_|_|_| |_|_  |
+# .                              |___|
+# ──────────────────────────────────────────────────────────────────────────────
+# A central flake for deploying all nix-based systems across multiple sites.
+
 {
-  description = "This flake manages all my nix machines.";
+  description = "Central flake for managing all nix deployments.";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -7,17 +15,20 @@
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
-  outputs =
-    inputs@{ self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
+    in {
       # The itx server hosting most of my homelab.
       nixosConfigurations.itxserver = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
         modules = [ ./hosts/itxserver/configuration.nix ];
+      };
+
+      nixosConfigurations.alfa = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./hosts/s-alfa/default.nix ];
       };
     };
 }
---
+++ hosts/common.nix
@@ -0,0 +1,40 @@
+# .                                       _   _
+#  ___ ___ _____ _____ ___ ___    ___ ___| |_|_|___ ___ ___
+# |  _| . |     |     | . |   |  | . | . |  _| | . |   |_ -|
+# |___|___|_|_|_|_|_|_|___|_|_|  |___|  _| | |_|___|_|_|___|
+# .                                  |_| |__|
+# ──────────────────────────────────────────────────────────────────────────────
+# Defines options that are common across all host systems, be that servers or
+#   clients.
+
+{ pkgs, ... }: {
+  # This is the default locale for any machine
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Common nix settings that apply to all machines
+  nix = {
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      auto-optimise-store = true;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+  };
+
+  # Basic packages that are nice to have on any machine with any user
+  environment.systemPackages = with pkgs; [
+    bat
+    btop
+    curl
+    fastfetch
+    git
+    nixfmt
+    tmux
+    tree
+    vim
+    wget
+  ];
+}
