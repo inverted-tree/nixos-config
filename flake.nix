50e6d0ea125b0c69edea32f1a86f55e6be9e4451
Lukas Schröder <lukasscr@proton.me>
2025-11-22 23:11:18 +0100

    Introduce fleet management with colmena
    This commit modifies the main flake to allow for the use of colmena as a
    fleet management tool.

--- flake.nix
+++ flake.nix
@@ -11,26 +11,58 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    colmena.url = "github:zhaofengli/colmena";
     sops-nix.url = "github:Mic92/sops-nix";
-    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
   };
 
   outputs =
-    inputs@{ self, nixpkgs, ... }:
+    inputs@{
+      self,
+      nixpkgs,
+      colmena,
+      ...
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      # The itx server hosting most of my homelab.
-      nixosConfigurations.itxserver = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [ ./hosts/itxserver/configuration.nix ];
+      # Define a local nix-develop environment
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ colmena.packages.${system}.colmena ];
       };
 
+      # Manage the entire fleet with colmena
+      colmenaHive = colmena.lib.makeHive {
+        meta = {
+          nixpkgs = import nixpkgs {
+            inherit system;
+          };
+          specialArgs = { inherit inputs; };
+        };
+
+        # The itx server hosting most of my homelab
+        alfa = {
+          deployment = {
+            targetHost = "100.89.38.72";
+            targetUser = "lukas";
+            tags = [ "homelab" ];
+          };
+
+          imports = [
+            ./hosts/itxserver/configuration.nix
+          ];
+        };
+      };
+
+      # Although the whole fleet can be managed with colmena, I like to have a
+      #   vanilla nixos fallback option in case colmena breaks.
+      # ----------------------------------------------------------------------
+
+      # The itx server hosting most of my homelab
       nixosConfigurations.alfa = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
-        modules = [ ./hosts/s-alfa/default.nix ];
+        modules = [ ./hosts/itxserver/configuration.nix ];
       };
     };
 }
--- hosts/itxserver/configuration.nix
+++ hosts/itxserver/configuration.nix
@@ -30,7 +30,6 @@ in
     ../../modules/containers/homeassistant.nix
     ../../modules/containers/plex.nix
     ../../modules/containers/freshrss.nix
-    ../../modules/other/minecraft-server.nix
     # Any other modules:
     inputs.sops-nix.nixosModules.sops
   ];
