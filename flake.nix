dc02e2d0a019ee4a01c1a21c336498d9cdb5bfae
Lukas Schröder <lukasscr@proton.me>
2025-11-24 12:51:36 +0100

    Expose the mkTools in the configs main flake
    The mkTools can now be called from the config's root (i.e. /etc/nixos).

--- flake.nix
+++ flake.nix
@@ -13,31 +13,34 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     colmena.url = "github:zhaofengli/colmena";
     sops-nix.url = "github:Mic92/sops-nix";
+
+    scripts.url = "path:./scripts";
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      colmena,
-      ...
-    }:
+  outputs = inputs@{ self, nixpkgs, colmena, scripts, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
+    in {
       # Define a local nix-develop environment
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ colmena.packages.${system}.colmena ];
+      devShells.${system} = {
+        # The default develop shell featuring colmena
+        default = pkgs.mkShell {
+          buildInputs = [ colmena.packages.${system}.colmena ];
+        };
+        # A separate develop shell featuring the generator tools
+        mkTools = scripts.devShells.${system}.default;
+      };
+      # To be able to run the generator tools without entering a dev shell we
+      #  export them as apps
+      apps.${system} = {
+        inherit (scripts.apps.${system}) mkHost mkSite mkUser;
       };
 
       # Manage the entire fleet with colmena
       colmenaHive = colmena.lib.makeHive {
         meta = {
-          nixpkgs = import nixpkgs {
-            inherit system;
-          };
+          nixpkgs = import nixpkgs { inherit system; };
           specialArgs = { inherit inputs; };
         };
 
@@ -49,9 +52,7 @@
             tags = [ "homelab" ];
           };
 
-          imports = [
-            ./hosts/alfa/default.nix
-          ];
+          imports = [ ./hosts/alfa/default.nix ];
         };
       };
 
